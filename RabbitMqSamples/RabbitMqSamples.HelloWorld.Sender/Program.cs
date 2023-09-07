246137690c8c011d3cf952aaf1a998aef2e40072
maleki <maleki@MASOUD>
2023-09-07 21:42:17 +0330

    Add project files.

---
+++ RabbitMqSamples/RabbitMqSamples.HelloWorld.Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var chanel = connection.CreateModel(); // use for multi Thread
+
+var queueName = "HelloWorld";
+chanel.QueueDeclare(queueName,false,false,false,null);
+
+Console.Write("Type your message and press [Enter]: ");
+var message = Console.ReadLine() ?? "Default message";
+var messageBytes = System.Text.Encoding.UTF8.GetBytes(message).ToArray();
+
+chanel.BasicPublish(string.Empty, queueName, null, messageBytes);
+
+Console.WriteLine($"[*] Your message sent: {message}");
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
+
+connection.Close();
+    
+
+
